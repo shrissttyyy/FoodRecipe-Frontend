f490351357dc4729b69cc9927c7c41f8cbc5fb75
shrissttyyy <shiristis8@gmail.com>
2025-08-19 10:47:06 +0545

    Move Pages workflow to repo root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: replace with your repo name
+  base: '/food-blog-app/', // e.g. '/FoodRecipe-frontend/'
 })
